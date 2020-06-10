22dec214a2408ce298a786c23032d4117cf9b9c4
lf87 <jongo1@gmail.com>
2020-06-11 00:17:40 +0100

    removed service worker (too many assets / not suitable)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './registerServiceWorker'
 import '@/assets/styles/style.scss'
 
 Vue.config.productionTip = false
