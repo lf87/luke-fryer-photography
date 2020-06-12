6dc4d1b6c6f0bb29a3b37298f99cad9a2d44ab33
lf87 <luke.fryer.email@gmail.com>
2020-06-13 00:36:45 +0100

    used async await and promise all to maintain the order of the data when fetching

--- src/App.vue
+++ src/App.vue
@@ -41,31 +41,40 @@ export default {
   }),
   created () {
     const iterator = this.categories.values()
-    let loopIndex = 0
-    const categoriesLength = this.categories.length
+    // let loopIndex = 0
+    // const categoriesLength = this.categories.length
+    // function objectKeys (obj) {
+    //   var keys = [];
+    //   if (!obj) return keys;
+    //   for (var key in obj) {
+    //     if (obj.hasOwnProperty(key)) {
+    //       keys.push(key);
+    //     }
+    //   }
+    // }
+    const promises = []
+    // let i = 1
+    const fn = async () => {
+      for (const value of iterator) {
+        promises.push(
+          fetch(`/data/categories/${value}.json`)
+            .then((response) =>
+              response.json()
+            )
+        )
+      }
 
-    for (const value of iterator) {
-      fetch(`/data/categories/${value}.json`)
-        .then((response) =>
-          response.json()
-            // eslint-disable-next-line no-console
-            .catch(err => { console.error(`'${err}' happened!`); return {} }))
-        .then((json) => {
-          this.data.push(json)
-
-          loopIndex++
-          if (loopIndex === categoriesLength) {
-            // Signal data fetched upon completion
-            this.dataFetched = true
-          }
-        })
+      await Promise.all(promises).then(data => {
+        console.log('666')
+        console.log(data)
+        this.data = data
+        this.dataFetched = true
+      })
     }
+
+    fn()
   },
   mounted () {
-    // window.addEventListener('load', () => {
-    //   this.transitionsActive = true
-    // })
-
     // Emitted from HomePage.vue
     EventBus.$on('transitionsActive', (bool) => {
       !bool ? this.transitionsActive = false : this.transitionsActive = true
